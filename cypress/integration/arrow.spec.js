21d96472e71864e3cc8d85b0df11059e66c9d287
YagoSilva <yago.silva@atsinformatica.com>
2022-12-12 19:03:36 -0300

    Aula 3 Arrow Functions

---
+++ cypress/integration/arrow.spec.js
@@ -0,0 +1,21 @@
+it('nada agora', function() { })
+
+//function soma(a, b){
+//    return a + b;
+//}
+
+//const soma = function (a, b) {
+//    return a + b;
+//}
+
+//const soma = (a, b) =>{
+//    return a + b;
+//}
+
+const soma = (a, b) => a + b
+
+console.log(soma(5,3))
+
+it('a function test...', function () {
+    console.log('Function', this)
+})
